319a8d63213876821bc23493e331522d949fbfad
Fernando Cortez <fernando.cortez@unity3d.com>
2022-02-11 16:02:01 -0500

    end to end test refined, utils class added for invoking buttons & loading scenes

--- Assets/Tests/BossRoomHostTest.cs
+++ Assets/Tests/BossRoomHostTest.cs
@@ -2,68 +2,85 @@
 using UnityEngine.TestTools;
 using System.Collections;
 using NUnit.Framework;
+using Unity.Multiplayer.Samples.BossRoom.TestUtils;
 using Unity.Netcode;
-using UnityEngine.EventSystems;
+using UnityEditor;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 namespace Unity.Multiplayer.Samples.BossRoom.RuntimeTests
 {
-    public class BossRoomHostTest : IPrebuildSetup
+    public class BossRoomHostTest
     {
-        public void Setup()
-        {
+        const string k_BootstrapScenePath = "Assets/BossRoom/Scenes/Startup.unity";
 
-        }
+        const string k_MainMenuScenePath = "Assets/BossRoom/Scenes/MainMenu.unity";
+
+        const string k_CharSelectScenePath = "Assets/BossRoom/Scenes/CharSelect.unity";
 
         [UnityTest]
         public IEnumerator NetworkManager_HostSmokeTest_True()
         {
-            // load Startup scene
-            SceneManager.LoadSceneAsync("Startup");
+            Assert.That(
+                System.Array.Exists(EditorBuildSettings.scenes, scene => scene.path == k_BootstrapScenePath));
 
-            while (SceneManager.GetActiveScene().name != "MainMenu")
-            {
-                yield return null;
-            }
+            // Bootstrap scene exists; load it
+            SceneManager.LoadSceneAsync(k_BootstrapScenePath);
+
+            var waitUntilStartupSceneLoaded = new WaitForSceneLoad(k_BootstrapScenePath);
+
+            yield return waitUntilStartupSceneLoaded;
+
+            Assert.That(!waitUntilStartupSceneLoaded.timedOut);
+
+            // MainMenu is loaded as soon as Startup scene is launched, validate it is loaded
+            var waitUntilMainMenuSceneLoaded = new WaitForSceneLoad(k_MainMenuScenePath);
+
+            yield return waitUntilMainMenuSceneLoaded;
+
+            Assert.That(!waitUntilMainMenuSceneLoaded.timedOut);
 
             // MainMenu scene
-            var hostButtonGameObject = GameObject.Find("Host Btn");
-            var hostButton = hostButtonGameObject.GetComponent<Button>();
-            hostButton.OnSubmit(new PointerEventData(EventSystem.current));
 
-            yield return null;
+            TestUtils.TestUtils.ClickButtonByName("Host Btn");
 
-            var confirmButtonGameObject = GameObject.Find("Confirmation Button");
-            var confirmButton = confirmButtonGameObject.GetComponent<Button>();
-            confirmButton.OnSubmit(new PointerEventData(EventSystem.current));
+            // if button is successfully clicked, a confirmation popup will appear; wait a frame for it to pop up
+            yield return new WaitForEndOfFrame();
 
-            yield return null;
+            TestUtils.TestUtils.ClickButtonByName("Confirmation Button");
+
+            // confirming hosting will initialize the hosting process; next frame the results will be ready
+            yield return new WaitForEndOfFrame();
 
             // verify hosting is successful
             Assert.That(NetworkManager.Singleton != null && NetworkManager.Singleton.IsListening);
 
-            while (SceneManager.GetActiveScene().name != "CharSelect")
-            {
-                yield return null;
-            }
+            // CharSelect is loaded as soon as hosting is successful, validate it is loaded
+            var waitUntilCharSelectSceneLoaded = new WaitForSceneLoad(k_CharSelectScenePath);
 
-            // select a character
-            var characterButtonGameObject = GameObject.Find("PlayerSeat (0)");
-            var characterButton = characterButtonGameObject.GetComponentInChildren<Button>();
-            characterButton.OnSubmit(new PointerEventData(EventSystem.current));
+            yield return waitUntilCharSelectSceneLoaded;
 
+            Assert.That(!waitUntilCharSelectSceneLoaded.timedOut);
+
+            // select first Character
+            // TODO: add option to test with all Avatar classes (ie. seats 0-7)
+            TestUtils.TestUtils.ClickButtonByName("ClickInteract0");
+
+            // selecting a class will enable the "Ready" button, next frame it is selectable
             yield return null;
 
             // hit ready
-            var readyButtonGameObject= GameObject.Find("Ready Btn");
-            var readyButton = readyButtonGameObject.GetComponentInChildren<Button>();
-            readyButton.OnSubmit(new PointerEventData(EventSystem.current));
+            TestUtils.TestUtils.ClickButtonByName("Ready Btn");
 
-            while (SceneManager.GetActiveScene().name != "BossRoom")
-            {
-                yield return null;
-            }
+            // selecting ready as host with no other party members will load BossRoom scene
+
+            var waitUntilBossRoomSceneLoaded = new WaitForSceneLoad("BossRoom");
+
+            yield return waitUntilBossRoomSceneLoaded;
+
+            Assert.That(!waitUntilBossRoomSceneLoaded.timedOut);
+
+            // once loaded into BossRoom scene, disconnect
+
 
             yield return new WaitForSeconds(10f);
         }
---
+++ Assets/Tests/Utils/TestUtils.cs
@@ -0,0 +1,64 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Unity.Multiplayer.Samples.BossRoom.TestUtils
+{
+    public class TestUtils
+    {
+        public static void ClickButtonByName(string name)
+        {
+            var buttonGameObject = GameObject.Find(name);
+
+            Assert.IsNotNull(buttonGameObject,
+                $"Button GameObject with name {name} not found in scene!");
+
+            EventSystem.current.SetSelectedGameObject(buttonGameObject);
+
+            var buttonComponent = buttonGameObject.GetComponent<Button>();
+
+            Assert.IsNotNull(buttonComponent, $"Button component not found on {buttonGameObject.name}!");
+
+            buttonComponent.onClick.Invoke();
+        }
+    }
+
+    public class WaitForSceneLoad : CustomYieldInstruction
+    {
+        const float k_MaxSceneLoadDuration = 10f;
+
+        string m_SceneName;
+
+        float m_LoadSceneStart;
+
+        float m_MaxLoadDuration;
+
+        public bool timedOut { get; private set; }
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                var scene = SceneManager.GetSceneByPath(m_SceneName);
+
+                var isSceneLoaded = scene.IsValid() && scene.isLoaded;
+
+                if (Time.time - m_LoadSceneStart >= m_MaxLoadDuration)
+                {
+                    timedOut = true;
+                }
+
+                return !isSceneLoaded && !timedOut;
+            }
+        }
+
+        public WaitForSceneLoad(string sceneName, float maxLoadDuration = k_MaxSceneLoadDuration)
+        {
+            m_LoadSceneStart = Time.time;
+            m_SceneName = sceneName;
+            m_MaxLoadDuration = maxLoadDuration;
+        }
+    }
+}
